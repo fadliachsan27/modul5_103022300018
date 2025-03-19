f24a4437bbc410d39f2e4899435bf6390e83c496
fadliachsan27 <fadliachsan010@gmail.com>
2025-03-19 07:21:36 +0700

    Menambahkan class PemrosesData dengan method  DapatkanNilaiTerbesar

---
+++ modul5_103022300018/PemrosesData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300018
+{
+    class PemrosesData
+    {
+        private object dynamic;
+
+        public void DapatkanNilaiTerbsesar<T>(T nilai1, T nilai2, T nilai3)
+        {
+            dynamic digit1 = (dynamic)nilai1;
+            dynamic digit2 = (dynamic)nilai2;
+            dynamic digit3 = (dynamic)nilai3;
+
+            if (digit1 > digit2 && digit1 > digit3)
+            {
+                Console.WriteLine("Nilai terbesar adalah " + digit1);
+            }
+            else if (digit2 > digit1 && digit2 > digit3)
+            {
+                Console.WriteLine("Nilai terbesar adalah " + digit2);
+            }
+            else
+            {
+                Console.WriteLine("Nilai terbesar adalah " + digit3);
+            }
+        }
+    }
+}
---
+++ modul5_103022300018/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PemrosesData proses = new PemrosesData();
+            proses.DapatkanNilaiTerbsesar<int>(30, 00, 18);
+        }
+    }
+}
